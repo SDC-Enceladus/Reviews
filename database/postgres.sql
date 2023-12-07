c34f3a3930c1bbd948a2744c4a1a441f09ecd90b
Gordon <gpchiu1213@gmail.com>
2023-12-07 15:46:35 -0800

    Forgot to save uncommented code

--- database/postgres.sql
+++ database/postgres.sql
@@ -16,56 +16,56 @@ CREATE TABLE "reviews" (
   "helpfulness" INT DEFAULT 0
 );
 
--- DROP TABLE IF EXISTS "characteristics";
+DROP TABLE IF EXISTS "characteristics";
 
--- CREATE TABLE "characteristics" (
---   "id" SERIAL PRIMARY KEY,
---   "product_id" INT,
---   "name" TEXT
--- );
+CREATE TABLE "characteristics" (
+  "id" SERIAL PRIMARY KEY,
+  "product_id" INT,
+  "name" TEXT
+);
 
--- DROP TABLE IF EXISTS "characteristics_reviews";
+DROP TABLE IF EXISTS "characteristics_reviews";
 
--- CREATE TABLE "characteristics_reviews" (
---   "id" SERIAL PRIMARY KEY,
---   "characteristic_id" INT,
---   FOREIGN KEY(characteristic_id)
---     REFERENCES characteristics(id),
---   "review_id" INT,
---   FOREIGN KEY(review_id)
---     REFERENCES reviews(id),
---   "value" SMALLINT CHECK (value > 0 AND value < 6)
--- );
+CREATE TABLE "characteristics_reviews" (
+  "id" SERIAL PRIMARY KEY,
+  "characteristic_id" INT,
+  FOREIGN KEY(characteristic_id)
+    REFERENCES characteristics(id),
+  "review_id" INT,
+  FOREIGN KEY(review_id)
+    REFERENCES reviews(id),
+  "value" SMALLINT CHECK (value > 0 AND value < 6)
+);
 
--- DROP TABLE IF EXISTS "reviews_photos";
+DROP TABLE IF EXISTS "reviews_photos";
 
--- CREATE TABLE "reviews_photos" (
---   "id" SERIAL PRIMARY KEY,
---   "review_id" INT,
---  FOREIGN KEY(review_id)
---     REFERENCES reviews(id),
---   "url" TEXT
--- );
+CREATE TABLE "reviews_photos" (
+  "id" SERIAL PRIMARY KEY,
+  "review_id" INT,
+ FOREIGN KEY(review_id)
+    REFERENCES reviews(id),
+  "url" TEXT
+);
 
 COPY reviews FROM
 '/private/tmp/reviews.csv'
 DELIMITER ','
 CSV HEADER;
 
--- COPY characteristics FROM
--- '/private/tmp/characteristics.csv'
--- DELIMITER ','
--- CSV HEADER;
+COPY characteristics FROM
+'/private/tmp/characteristics.csv'
+DELIMITER ','
+CSV HEADER;
 
--- COPY characteristics_reviews FROM
--- '/private/tmp/characteristic_reviews.csv'
--- DELIMITER ','
--- CSV HEADER;
+COPY characteristics_reviews FROM
+'/private/tmp/characteristic_reviews.csv'
+DELIMITER ','
+CSV HEADER;
 
--- COPY reviews_photos FROM
--- '/private/tmp/reviews_photos.csv'
--- DELIMITER ','
--- CSV HEADER;
+COPY reviews_photos FROM
+'/private/tmp/reviews_photos.csv'
+DELIMITER ','
+CSV HEADER;
 
 -- DROP VIEW IF EXISTS metadata;
 -- CREATE VIEW metadata AS
